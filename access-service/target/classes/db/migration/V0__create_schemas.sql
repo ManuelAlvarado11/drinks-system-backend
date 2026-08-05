758931cfe4b01e18288d50565a462a63af2b947e
Manuel Alvarado <manuelalvarado011@gmail.com>
2026-08-04 23:36:47 -0600

    Remover carpeta target del rastreo de Git

--- access-service/target/classes/db/migration/V0__create_schemas.sql
+++
@@ -1,7 +0,0 @@
--- V0__create_schemas.sql
--- Creación de esquemas para la base de datos drinks_db
-
-CREATE SCHEMA IF NOT EXISTS access;
-CREATE SCHEMA IF NOT EXISTS sales;
-CREATE SCHEMA IF NOT EXISTS inventory;
-CREATE SCHEMA IF NOT EXISTS reporting;
